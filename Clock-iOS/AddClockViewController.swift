1afc0cb92b1edd8fed6a3f168cba6379f14aecf3
jojos38 <jojos38000@hotmail.com>
2021-11-18 09:30:54 +0100

    Added second page
    Added second page

---
+++ Clock-iOS/AddClockViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddClockViewController.swift
+//  Clock-iOS
+//
+//  Created by Julien Cohard on 18/11/2021.
+//
+
+import UIKit
+
+class AddClockViewController: UIViewController {
+
+    @IBOutlet var addClockView: UIPageViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func openAddClock(_ sender: Any) {
+        
+        print("ok")
+        
+    }
+
+}
+
--- Clock-iOS/ViewController.swift
+++ Clock-iOS/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func openAddClock(_ sender: Any) {
+        
+        print("ok")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let clockVC = storyboard.instantiateViewController(identifier: "AddClockViewController") as! AddClockViewController
+        self.navigationController?.pushViewController(clockVC, animated: true)
+        
+    }
 
 }
 
