091342161b9481c8587720e0dabe3dd7273808fa
jojos38 <jojos38000@hotmail.com>
2021-11-18 10:56:08 +0100

    f

--- Clock-iOS/AddClockViewController.swift
+++ Clock-iOS/AddClockViewController.swift
@@ -8,16 +8,27 @@
 import UIKit
 
 class AddClockViewController: UIViewController {
-    
+
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var clockNameText: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+
     @IBAction func saveClock(_ sender: UIBarButtonItem) {
         print("sauvergarder")
+
+        let time: Date = datePicker.date
+        let name: String = clockNameText.text ?? ""
+
+        // let dateFormatter = DateFormatter()
+        // dateFormatter.dateFormat = "HH:mm"
+        // print(dateFormatter.string(from: time))
+        
+        print(time)
+        print(name)
     }
-    
-    
 }
 
